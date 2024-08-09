83c90a6ef483fe8d50b26820ad78b0b9385b0d82
rameshelamathi <ramesh@cartrabbit.in>
2024-08-10 00:34:46 +0530

    - Initial commit

---
+++ install-db.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Source the DB variables from the env.
+source ./env.sh
+
---
+++ start-mailpit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Source the DB variables from the env.
+source ./env.sh
+
+./mailpit
